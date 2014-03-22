87e09b01faa0d4ac337d14649516c324d7ec6c40
k <com_sc511463041@hotmail.com>
2014-03-22 15:58:35 +0700

    เพิ่มไฟล์ index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	/*
+		VDO 1 
+	*/
+	class firstClass{
+
+		public $name = 'sunthon1';
+		public $surname = 'chaiwongton';
+		public function sayHi(){
+			/*
+				self::$name; = $this->name;
+			*/
+			 $nickname = '(k)';
+			echo "Hi ".$this->name.' '.$this->surname.' '.$nickname.$this->sayBye();
+		}
+
+		public function sayBye(){
+			return "<br>GoodBye";
+		}
+
+		public function sayHello(){
+			echo 'Hello<br>';
+		}
+	}
+	
+
+	$obj= new firstClass;
+	$obj->surname = 'chaiwongton2';
+	$obj->name = 'sunthon';
+	$obj->sayHi();
+
+	
+	
+
+?>
